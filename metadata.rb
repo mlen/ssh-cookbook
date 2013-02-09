a24d3468edbeee2cf8db3bc3a4a82334c9a3e732
Mateusz Lenik <mt.lenik@gmail.com>
2013-02-09 20:21:23 +0100

    Added agent forwarding

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Mateusz Lenik"
 maintainer_email  "mt.lenik@gmail.com"
 license           "WTFPL"
 description       "Installs and configures ssh"
-version           "1.0.0"
+version           "1.0.1"
 
 recipe "ssh", "Installs and configures both SSH client and server"
 recipe "ssh::client", "Installs and configures SSH client"
