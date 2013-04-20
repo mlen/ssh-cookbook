827d80ae2c96f0cf7e6b81386f3eda68b6b4adb5
Mateusz Lenik <mt.lenik@gmail.com>
2013-04-20 22:50:44 +0200

    Added IPv6 firewall rules

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Mateusz Lenik"
 maintainer_email  "mt.lenik@gmail.com"
 license           "WTFPL"
 description       "Installs and configures ssh"
-version           "1.0.1"
+version           "1.1.0"
 
 recipe "ssh", "Installs and configures both SSH client and server"
 recipe "ssh::client", "Installs and configures SSH client"
