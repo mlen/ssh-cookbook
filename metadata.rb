3a5707b9845bb4ec539c7843ed5a46c3b80a6fa4
Mateusz Lenik <mlen@mlen.pl>
2014-12-17 19:53:40 +0100

    Added support for non-debian based OSes

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,14 @@ maintainer        "Mateusz Lenik"
 maintainer_email  "mt.lenik@gmail.com"
 license           "WTFPL"
 description       "Installs and configures ssh"
-version           "1.2.0"
+version           "1.3.0"
 
 recipe "ssh", "Installs and configures both SSH client and server"
 recipe "ssh::client", "Installs and configures SSH client"
 recipe "ssh::server", "Installs and configures SSH server"
 
 supports "debian"
+supports "ubuntu"
+supports "centos"
+supports "fedora"
 
--- recipes/server.rb
+++ recipes/server.rb
@@ -8,15 +8,21 @@ template "/etc/ssh/sshd_config" do
   mode  "0644"
 
   source    "sshd_config.erb"
-  variables :settings   => defaults['server'],
-            :keep_alive => defaults['keep_alive'],
-            :timeout    => defaults['timeout']
+  variables settings:   defaults['server'],
+            keep_alive: defaults['keep_alive'],
+            timeout:    defaults['timeout']
 
   notifies :restart, "service[ssh]"
 end
 
 service "ssh" do
-  supports :restart => true, :reload => true, :status => true
+  case node[:platform].downcase
+  when 'debian', 'ubuntu'
+    service_name 'ssh'
+  else
+    service_name 'sshd'
+  end
+
+  supports restart: true, reload: true, status: true
   action   [:enable, :start]
 end
-
